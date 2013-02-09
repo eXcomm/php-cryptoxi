bbef64d6f79c120e3a2ba0202b5ae7656a32ba3e
Logan Howlett <logan@weaponxi.com>
2013-02-09 02:43:00 -0500

    added jcryption to js and crypt.php

--- src/crypt.php
+++ src/crypt.php
@@ -1,55 +1,49 @@
 <?php
-function hexit ($str) {
-	return bin2hex($str);
-}
-function binit ($str){
-	return pack("H*",$str);
-	
-}
-/*
-Hex to bin - bin to hex conversion example
-===========================================
-*/
-
-#echo hexit('brown fox'); echo '</br> binit: '; echo binit('62726f776e20666f78'); echo '</br>';
-
-#==========================================
-
-function mysql_aes_encrypt($val,$ky) 
-{ 
-    $key="2c3fec85b5f6b0a1cd5a1010c67eb772"; 
-    for($a=0;$a<strlen($ky);$a++) 
-      $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a])); 
-    $mode=MCRYPT_MODE_ECB; 
-    $enc=MCRYPT_RIJNDAEL_128; 
-    $val=str_pad($val, (16*(floor(strlen($val) / 16)+(strlen($val) % 16==0?2:1))), chr(16-(strlen($val) % 16))); 
-    $result = mcrypt_encrypt($enc, $key, $val, $mode, mcrypt_create_iv( mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM)); 
-	return hexit($result);
-} 
-
-
-function mysql_aes_decrypt($val,$ky) 
-{ 	
-	$value = binit($val);
-    $key="2c3fec85b5f6b0a1cd5a1010c67eb772"; 
-    for($a=0;$a<strlen($ky);$a++) 
-      $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a])); 
-    $mode = MCRYPT_MODE_ECB; 
-    $enc = MCRYPT_RIJNDAEL_128; 
-    $dec = @mcrypt_decrypt($enc, $key, $value, $mode, @mcrypt_create_iv( @mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM ) ); 
-    return rtrim($dec,(( ord(substr($dec,strlen($dec)-1,1))>=0 and ord(substr($dec, strlen($dec)-1,1))<=16)? chr(ord( substr($dec,strlen($dec)-1,1))):null)); 
-} 
-$key = $_POST['key'];
-$crypt = $_POST['tocrypt'];
-
-
-$encrypted =  mysql_aes_encrypt ($crypt,$key);
-
-echo $encrypted; 
-
-if($_POST['todecrypt']!==''){
-	$decrypt = $_POST['todecrypt']; $decrypted = 'x-.-x'.mysql_aes_decrypt($decrypt,$key);
-	 echo $decrypted; }
-else {echo 'x-.-xDecrypted text will appear here.';}
-
+    // Start the session so we can use PHP sessions
+    session_start();
+    // Include the jCryption library
+    require_once("../../jcryption.php");
+    // Set the RSA key length
+    $keyLength = 1024;
+    // Create a jCrytion object
+    $jCryption = new jCryption();
+    
+    // If the GET parameter "generateKeypair" is set
+    if(isset($_GET["generateKeypair"])) {
+        // Include some RSA keys
+        require_once("../../100_1024_keys.inc.php");
+        // Pick a random RSA key from the array
+        $keys = $arrKeys[mt_rand(0, 100)];
+        // Save the RSA keypair into the session
+        $_SESSION["e"] = array("int" => $keys["e"], "hex" => $jCryption->dec2string($keys["e"], 16));
+        $_SESSION["d"] = array("int" => $keys["d"], "hex" => $jCryption->dec2string($keys["d"], 16));
+        $_SESSION["n"] = array("int" => $keys["n"], "hex" => $jCryption->dec2string($keys["n"], 16));
+        // Create an array containing the RSA keypair
+        $arrOutput = array(
+            "e" => $_SESSION["e"]["hex"],
+            "n" => $_SESSION["n"]["hex"],
+            "maxdigits" => intval($keyLength*2/16+3)
+        );
+        // JSON encode the RSA keypair
+        echo json_encode($arrOutput);
+    // If the GET parameter "handshake" is set
+    } elseif (isset($_GET["handshake"])) {
+        // Decrypt the AES key with the RSA key
+        $key = $jCryption->decrypt($_POST['key'], $_SESSION["d"]["int"], $_SESSION["n"]["int"]);
+        // Removed the RSA key from the session
+        unset($_SESSION["e"]);
+        unset($_SESSION["d"]);
+        unset($_SESSION["n"]);
+        // Save the AES key into the session
+        $_SESSION["key"] = $key;
+        // JSON encohe the challenge
+        echo json_encode(array("challenge" => AesCtr::encrypt($key, $key, 256)));
+    } else {
+        // Decrypt the request data
+        $decryptedData = AesCtr::decrypt($_POST['jCryption'], $_SESSION["key"], 256);
+        // Encrypt it again for testing purposes
+        $encryptedData = AesCtr::encrypt($decryptedData, $_SESSION["key"],256);
+        // JSON encode the response
+        echo json_encode(array("data" => $encryptedData));
+    }
 ?>
--- src/main.coffee
+++ src/main.coffee
@@ -4,3 +4,92 @@ output = $('#output')
 passphrase = $('#pass')
 online = $('#online')
 
+# Loader gif image string
+$loader = $("<img src=\"loading.gif\" alt=\"Loading...\" title=\"Loading...\" style=\"margin-right:15px;\" />")
+$ ->
+  
+  ###
+  Creates a random string
+  @returns {string} A random string
+  ###
+  randomString = ->
+    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
+    string_length = 128
+    randomstring = ""
+    i = 0
+
+    while i < string_length
+      rnum = Math.floor(Math.random() * chars.length)
+      randomstring += chars.substring(rnum, rnum + 1)
+      i++
+    
+    #randomstring += cursor.x;
+    #randomstring += cursor.y;
+    randomstring
+  
+  # Initialize the password variable
+  password = undefined
+  
+  # If a connection hasn't been made
+  unless sessionStorage.isConnected
+    
+    # Create a random AES key
+    hashObj = new jsSHA(randomString(), "ASCII")
+    password = hashObj.getHash("SHA-512", "HEX")
+    
+    # Authenticate with the server
+    $.jCryption.authenticate password, "encrypt.php?generateKeypair=true", "encrypt.php?handshake=true", ((AESKey) ->
+      
+      # Enable the buttons and the textfield
+      $("#text, #send,#clearSessionStorage").attr "disabled", false
+      $("#status").html "<span style=\"font-size: 16px;\">Let's Rock!</span>"
+      
+      # Save the current AES key into the sessionStorage
+      sessionStorage.setItem "isConnected", "1"
+      sessionStorage.setItem "password", password
+    ), ->
+      
+      # Authentication failed
+      alert "Authentication failed"
+
+  else
+    
+    # Enable the buttons and the textfield
+    $("#text, #send,#clearSessionStorage").attr "disabled", false
+    $("#status").html "<span style=\"font-size: 16px;\">Let's Rock!</span>"
+    
+    # Store the password from sessionStorage in the password variables
+    password = sessionStorage.password
+  $("#send").click ->
+    
+    # Encrypt the data with the AES key
+    encryptedString = $.jCryption.encrypt($("#text").val(), password)
+    
+    # logging
+    $("#log").prepend("\n").prepend "----------"
+    $("#log").prepend("\n").prepend "Plaintext: " + $("#text").val()
+    $("#log").prepend("\n").prepend "Encrypted: " + encryptedString
+    
+    # Send the data to the server
+    $.ajax
+      url: "encrypt.php?"
+      dataType: "json"
+      type: "POST"
+      data:
+        jCryption: encryptedString
+
+      success: (response) ->
+        
+        # Logging
+        $("#log").prepend("\n").prepend "Served sent: " + response.data
+        $("#log").prepend("\n").prepend "Decrypted: " + $.jCryption.decrypt(response.data, password)
+
+
+  $("#clearSessionStorage").click (e) ->
+    
+    # Clear the session storage
+    sessionStorage.clear()
+    
+    # Refresh the page
+    window.location = window.location
+
