4b5e4baff49cc81b47780948a51361826514e2b8
Logan Howlett <logan@weaponxi.com>
2013-02-09 20:07:59 -0500

    working on users

--- src/anon_userxi.php
+++ src/anon_userxi.php
@@ -1,3 +1,30 @@
 <?PHP
+require_once 'mysql.config.php';
+class UserXI {
+    var $user, $pass;
+    function UserXI ($user, $pass) {
+       $this->$user = md5($user);
+       $this->pass = md5($pass); 
+    }
+    private function save_cookie (){
 
+    }
+    private function login (){
+        $u = $this->user;
+        $p = $this->pass;
+        // Look for entries in database 
+        // Find ones that are max day old
+        $mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
+        if ($mysqli->connect_error) {
+            die('Connect Error (' . $mysqli->connect_errno . ') '
+                    . $mysqli->connect_error);
+        }
+        // See if found any users with the same password
+
+    }
+
+    function logout (){
+
+    }
+}
 ?>
