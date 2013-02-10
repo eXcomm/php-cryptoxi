0ada4562752842fd7dc7f01198a22260fbca66a1
Logan Howlett <logan@weaponxi.com>
2013-02-10 02:48:08 -0500

    fixed ids

--- src/sign_in_out.php
+++ src/sign_in_out.php
@@ -2,10 +2,10 @@
 require_once 'anon_userxi.php';
 $uxi = new UserXI();
 if ($uxi->is_logged()) {
-    $login_logout = '<li><a href="#">Logout</a></li>';
+    $login_logout = '<li><a href="#" id="logout">Logout</a></li>';
     $avatar = $uxi->avatar_url();
     $register_avatar =  '<li class="dropdown">
-              <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+              <a class="dropdown-toggle" href="#" data-toggle="dropdown">Avatar <strong class="caret"></strong></a>
               <div class="dropdown-menu" style="padding: 15px; padding-bottom: 15px;">
                <img src="'.$avatar.'">
               </div>
@@ -16,18 +16,16 @@ if ($uxi->is_logged()) {
               <div class="dropdown-menu" style="padding: 15px; padding-bottom: 15px;">
                 <form>
                   <input type="text" class="span2" placeholder="Login">
-                  <input type="text" class="span2" placeholder="Password">
-                  <button class="btn">Login</button>
+                  <input type="password" class="span2" placeholder="Password">
+                  <button id="login" class="btn">Login</button>
                 </form>
               </div>
             </li>';
-    $register_avatar = "Generate User";
+    $register_avatar = '<li><a href="#" id="register">Generate User</a></li>';
 }
 
 
 
-$form = '<li><a href="#">'.$register_avatar.'</a></li>
-            <li class="divider-vertical"></li>
-            '.$login_logout;
+$form = $register_avatar .'<li class="divider-vertical"></li>'.$login_logout;
 echo $form;
 ?>
